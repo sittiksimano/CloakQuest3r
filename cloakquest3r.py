26ef6a18974009c085c49663788bb1981073e951
Abubakkar Khan <sittiksimano@gmail.com>
2023-12-10 19:59:16 +0600

    Update cloakquest3r.py

--- cloakquest3r.py
+++ cloakquest3r.py
@@ -1,67 +1,67 @@
 import socket
-import sys
+import sys 
 import requests
 import threading
-from ipaddress import ip_address, AddressValueError
+from ipaddress import ip_address, AddressValueError  
 from colorama import init, Fore
 import argparse
 
 # Set up argparse for handling command line options
 parser = argparse.ArgumentParser(description="Find subdomains and detect if site uses Cloudflare")
-parser.add_argument("domain", help="The domain to scan")  
+parser.add_argument("domain", help="The domain to scan") 
 parser.add_argument("-w", "--wordlist", default="subdomains.txt", help="Wordlist of subdomains to check")
 parser.add_argument("-o", "--output", help="Save results to output file")
 parser.add_argument("-t", "--threads", type=int, default=10, help="Number of threads")
-
+  
 init()
 
 def check_subdomain(subdomain):
     url = f"https://{subdomain}.{domain}"
-    try:
-        resp = requests.head(url, timeout=5)
-        if resp.status_code == 200: 
-            print(f"[+] Found: {url} ")
-            with results_lock:
-                results.append(subdomain) 
+    try: 
+        resp = requests.head(url, timeout=5) 
+        if resp.status_code == 200:
+           print(f"[+] Found: {url} ") 
+           with results_lock: 
+               results.append(subdomain)
     except:
         pass
 
-if __name__ == "__main__":
+if __name__ == "__main__": 
 
     args = parser.parse_args()
-    domain = args.domain  
-    wordlist = args.wordlist
+    domain = args.domain
+    wordlist = args.wordlist 
     output_file = args.output
     num_threads = args.threads
     
     # Input validation
-    if not domain:  
+    if not domain:
         print("Error: No domain specified")
-        sys.exit(1)
+        sys.exit(1) 
     
     try:
         ip = socket.gethostbyname(domain) 
-    except socket.gaierror:
+    except socket.gaierror:  
         print("Error: Invalid domain name")
         sys.exit(1)
 
-    print(f"Scanning {domain} for subdomains...")
-   
+    print(f"Scanning {domain} for subdomains...")  
+    
     results = []
     results_lock = threading.Lock()
 
-    # Start threads 
+    # Start threads
     threads = []
     for subdomain in open(wordlist):
-        t = threading.Thread(target=check_subdomain,  args=[subdomain.strip()]) 
-        threads.append(t)
+        t = threading.Thread(target=check_subdomain,  args=[subdomain.strip()])
+        threads.append(t) 
         t.start()
 
-    # Wait for all threads to complete
+    # Wait for all threads to complete  
     for t in threads:
         t.join()
 
-    # Print summary and save results if output file is set
+    # Print summary and save results if output file is set 
     print(f"{len(results)} subdomains found")
     
     if output_file:
